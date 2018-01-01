80c619454da4c85c639d58ed4b27bb71537fb607
Martin Moene <martin.moene@gmail.com>
2018-01-01 15:01:00 +0100

    Expand Travis and Appveyor CI compiler matrices

--- test/string-view-lite.t.h
+++ test/string-view-lite.t.h
@@ -10,7 +10,19 @@
 #define TEST_SV_LITE_H_INCLUDED
 
 #include nssv_STRING_VIEW_HEADER
+
+// Limit C++ Core Guidelines checking to GSL Lite:
+
+#if defined(_MSC_VER) && _MSC_VER >= 1910
+# include <CppCoreCheck\Warnings.h>
+# pragma warning(disable: ALL_CPPCORECHECK_WARNINGS)
+#endif
+
 #include "lest_cpp03.hpp"
+
+#if nssv_USES_STD_STRING_VIEW
+# define nssv_nullptr  nullptr
+#endif
 
 using namespace nonstd;
 
