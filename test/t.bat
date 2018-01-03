73af0c597054e8233467e2b455bffda2c1fe9d24
Martin Moene <martin.moene@gmail.com>
2018-01-03 13:12:25 +0100

    Add include directory for MS C++ Core Guidelines checker

--- test/t.bat
+++ test/t.bat
@@ -1,6 +1,6 @@
 @setlocal
 @set std=%1
 @if not "%std%"=="" set std=-std:%std%
-cl -W3 -EHsc %std% -DNOMINMAX -Dnssv_STRING_VIEW_HEADER=\"../include/nonstd/string_view.hpp\" -I../include/nonstd string-view-lite.t.cpp string-view.t.cpp && string-view-lite.t.exe
+set cppcorecheckinclude=%VCINSTALLDIR%/Auxiliary/VS/include
+cl -W3 -EHsc %std% -Dnssv_CONFIG_SELECT_STD_STRING_VIEW=0 -Dnssv_CONFIG_SELECT_NONSTD_STRING_VIEW=1 -DNOMINMAX -Dnssv_STRING_VIEW_HEADER=\"string_view.hpp\" -I../include/nonstd -I"%cppcorecheckinclude%" string-view-lite.t.cpp string-view.t.cpp && string-view-lite.t.exe
 @endlocal
-
