3ca225376cfe14368d9da732253a03df8bfd02dd
Neargye <neargye@gmail.com>
2018-08-06 20:48:32 +0500

    add pragma once

--- include/nonstd/string_view.hpp
+++ include/nonstd/string_view.hpp
@@ -6,6 +6,8 @@
 // Distributed under the Boost Software License, Version 1.0. 
 // (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#pragma once
+
 #ifndef NONSTD_SV_LITE_H_INCLUDED
 #define NONSTD_SV_LITE_H_INCLUDED
 
