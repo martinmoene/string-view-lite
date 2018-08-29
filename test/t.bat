5b39bba25074961afebf2614448e9dee34688f85
Martin Moene <martin.moene@gmail.com>
2018-08-29 23:19:36 +0200

    Obviate need for -DNOMINMAX with MSVC (nonstd-lite issue 16)

--- test/t.bat
+++ test/t.bat
@@ -2,5 +2,5 @@
 @set std=%1
 @if not "%std%"=="" set std=-std:%std%
 set cppcorecheckinclude=%VCINSTALLDIR%/Auxiliary/VS/include
-cl -W3 -EHsc %std% -Dnssv_CONFIG_SELECT_STD_STRING_VIEW=0 -Dnssv_CONFIG_SELECT_NONSTD_STRING_VIEW=1 -Dnssv_CONFIG_STD_SV_OPERATOR=1 -Dnssv_CONFIG_USR_SV_OPERATOR=1 -DNOMINMAX -Dnssv_STRING_VIEW_HEADER=\"string_view.hpp\" -I../include/nonstd -I"%cppcorecheckinclude%" string-view-lite.t.cpp string-view.t.cpp && string-view-lite.t.exe
+cl -W3 -EHsc %std% -Dnssv_CONFIG_SELECT_STD_STRING_VIEW=0 -Dnssv_CONFIG_SELECT_NONSTD_STRING_VIEW=1 -Dnssv_CONFIG_STD_SV_OPERATOR=1 -Dnssv_CONFIG_USR_SV_OPERATOR=1 -Dnssv_STRING_VIEW_HEADER=\"string_view.hpp\" -I../include/nonstd -I"%cppcorecheckinclude%" string-view-lite.t.cpp string-view.t.cpp && string-view-lite.t.exe
 @endlocal
