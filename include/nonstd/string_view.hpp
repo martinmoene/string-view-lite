c1c3a576fcdd26aa498f767bff4e03d577840499
Martin Moene <martin.moene@gmail.com>
2020-10-03 21:27:00 +0200

    Release 1.5.0

--- include/nonstd/string_view.hpp
+++ include/nonstd/string_view.hpp
@@ -12,7 +12,7 @@
 #define NONSTD_SV_LITE_H_INCLUDED
 
 #define string_view_lite_MAJOR  1
-#define string_view_lite_MINOR  4
+#define string_view_lite_MINOR  5
 #define string_view_lite_PATCH  0
 
 #define string_view_lite_VERSION  nssv_STRINGIFY(string_view_lite_MAJOR) "." nssv_STRINGIFY(string_view_lite_MINOR) "." nssv_STRINGIFY(string_view_lite_PATCH)
