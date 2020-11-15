31934cc97d387fd59d170ac76a3581b7d81f879d
Martin Moene <martin.moene@gmail.com>
2020-11-15 22:24:03 +0100

    Replace _CPPUNWIND with _HAS_EXCEPTIONS (thanks @DBJDBJ)
    See also https://github.com/martinmoene/nonstd-lite-project/issues/48

--- include/nonstd/string_view.hpp
+++ include/nonstd/string_view.hpp
@@ -72,7 +72,10 @@
 // Control presence of exception handling (try and auto discover):
 
 #ifndef nssv_CONFIG_NO_EXCEPTIONS
-# if defined(__cpp_exceptions) || defined(__EXCEPTIONS) || defined(_CPPUNWIND)
+# if _MSC_VER
+# include <cstddef>     // for _HAS_EXCEPTIONS
+# endif
+# if defined(__cpp_exceptions) || defined(__EXCEPTIONS) || (_HAS_EXCEPTIONS)
 #  define nssv_CONFIG_NO_EXCEPTIONS  0
 # else
 #  define nssv_CONFIG_NO_EXCEPTIONS  1
